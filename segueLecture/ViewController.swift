04491dfeaedade408eb7b9b585773f43ae3245ec
alex <aboguszewski2037@stu.d214.org>
2019-11-26 11:46:06 -0600

    Initial Commit

---
+++ segueLecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  segueLecture
+//
+//  Created by alex boguszewski on 11/26/19.
+//  Copyright © 2019 alex boguszewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
